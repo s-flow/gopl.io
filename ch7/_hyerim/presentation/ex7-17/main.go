e91cf0e55adbab23da2501324acf27f684221766
김혜림(WT) <hyerim@wemade.com>
2020-02-06 18:06:21 +0900

    mid

--- ch7/_hyerim/presentation/ex7-17/main.go
+++ ch7/_hyerim/presentation/ex7-17/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	dec := xml.NewDecoder(os.Stdin)
-	var stack []string
+	var stack [][]string
 	for {
 		tok, err := dec.Token()
 		if err == io.EOF {
@@ -22,24 +22,28 @@ func main() {
 
 		switch tok := tok.(type) {
 		case xml.StartElement:
-			stack = append(stack, tok.Name.Local) // push
+			elem := []string{tok.Name.Local}
+			for _, attr := range tok.Attr {
+				elem = append(elem, attr.Value)
+			}
+			stack = append(stack, elem) // push
 		case xml.EndElement:
 			stack = stack[:len(stack)-1]
 		case xml.CharData:
 			if containsAll(stack, os.Args[1:]) {
-				fmt.Printf("%s : %s\n", strings.Join(stack, " "), tok)
+				fmt.Printf("%s : %s\n", strings.Join(flatten(stack), " "), tok)
 			}
 		}
 	}
 }
 
-func containsAll(x, y []string) bool {
+func containsAll(x [][]string, y []string) bool {
 	for len(y) <= len(x) {
 		if len(y) == 0 {
 			return true
 		}
 
-		if x[0] == y[0] {
+		if matches(x[0], y[0]) {
 			y = y[1:]
 		}
 
@@ -48,4 +52,28 @@ func containsAll(x, y []string) bool {
 	return false
 }
 
-// ./fetch http://www.w3.org/TR/2006/REC-xml11-20060816 | ./xmlselect div div h2
+func matches(x []string, y string) bool {
+	for _, elem := range x {
+		if elem == y {
+			return true
+		}
+	}
+	return false
+}
+
+func flatten(stack [][]string) []string {
+	s := make([]string, 0)
+	for _, elem := range stack {
+		var token string
+		if len(elem) > 1 {
+			token = elem[0] + "(" + strings.Join(elem[1:], " ") + ")"
+		} else {
+			token = elem[0]
+		}
+		s = append(s, token)
+	}
+
+	return s
+}
+
+// ./fetch http://www.w3.org/TR/2006/REC-xml11-20060816 | go run . div div1 enumar li p span
